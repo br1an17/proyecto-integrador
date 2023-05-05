3ba0a7b4773ed81fa46ed088165fd238fec99e9f
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-05-04 21:34:56 -0300

    prueva de fumcion

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
- document.getElementById("b").addEventListener("click",function(){
-alert("Si quieres ser un orador llena el formulario debajo")
-})
+let comprarTicket = document.getElementById("b2")
+
+function cambio () {
+alert("hola")
+}
+
+comprarTicket.addEventListener("click",cambio
+)
