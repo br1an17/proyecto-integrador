8814502685e09c5d5beb4cde5a0213480de0b279
br1an17 <106201381+br1an17@users.noreply.github.com>
2023-05-07 17:29:28 -0300

    cambios de app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let comprarTicket2 = document.getElementById("verde");
 let serOrador2 = document.getElementById("confe");
 let serOrador = document.getElementById("b");
 let ticket = document.querySelector(".modal")
+let cerrar = document.querySelector(".btn-close")
 
 console.log(ticket.style)
 
@@ -10,13 +11,18 @@ function compra() {
   ticket.style.display = "block" 
 }
 
+function cerrarModal() {
+  ticket.style.display = "none" 
+
+}
+
+
 function orador() {
-    alert("Llene el formulario al pie de la pagina");
-  }
+  alert("Llene el formulario al pie de la pagina");  }
 
 
 comprarTicket.addEventListener("click", compra);
 comprarTicket2.addEventListener("click", compra);
 serOrador.addEventListener("click",orador);
 serOrador2.addEventListener("click",orador);
-
+cerrar.addEventListener("click",cerrarModal)
